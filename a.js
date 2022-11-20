af083b8302fa44ddd9758824e1750cd05647e5c5
lilu <1836923239@qq.com>
2022-11-20 16:16:43 +0800

    first

---
+++ a.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: lilu 1836923239@qq.com
+ * @Date: 2022-11-20 16:16:23
+ * @LastEditors: lilu 1836923239@qq.com
+ * @LastEditTime: 2022-11-20 16:16:31
+ * @FilePath: /haha/a.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const a ={};
